fd006149d0f55a8aa91a4006aa1a7b7c92c4adca
Jindřich Pergler <jindrich@pergler.eu>
2022-10-25 16:46:28 +0200

    Add JayToolbox.Integrations.Jira project with IssueExtensions.cs

---
+++ src/JayToolbox.Integrations.Jira/IssueExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Atlassian.Jira;
+
+namespace JayToolbox.Integrations.Jira;
+
+public static class IssueExtensions
+{
+    public static T? GetCustomField<T>(this Issue issue, string fieldName)
+    {
+        var value = issue.CustomFields[fieldName]?.Values.FirstOrDefault();
+        if (value == null) return default;
+        return (T?)Convert.ChangeType(value, typeof(T));
+    }
+}
